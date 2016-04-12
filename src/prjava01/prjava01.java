fe5e95af49f1cdfb06a037aaaa6b90e0d8257b9d
Eloi Colom Salvans <elcosa1516daw2@gmail.com>
2016-04-12 21:26:07 +0200

    Versió 0.2 del projecte prjava01

--- src/prjava01/prjava01.java
+++ src/prjava01/prjava01.java
@@ -26,6 +26,7 @@ public class prjava01 {
 			bw.write("  </head>");
 			bw.newLine();
 			bw.write("  <body>");
+			bw.write("    <h1>DAW2 m08uf4pr2</h1>");bw.newLine();
 			bw.newLine();
 			bw.newLine();
 			bw.write("    Nova p&agrave;gina web");
